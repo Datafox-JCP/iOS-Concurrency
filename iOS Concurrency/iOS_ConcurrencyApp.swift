4b84c11471b02174fb59697a6a241b682cd9464e
Datafox-JCP <datafox@outlook.com>
2023-03-08 12:45:39 -0600

    Initial Commit

---
+++ iOS Concurrency/iOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ConcurrencyApp.swift
+//  iOS Concurrency
+//
+//  Created by Juan Hernandez Pazos on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
